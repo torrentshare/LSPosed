97046f03c85114123a8321f60c111c4620b9985c
PureQing <35628468+CMDQ8575@users.noreply.github.com>
2023-01-17 14:10:31 +0800

    Change Magisk Update Channel

--- magisk-loader/build.gradle.kts
+++ magisk-loader/build.gradle.kts
@@ -157,7 +157,7 @@ fun afterEval() = android.applicationVariants.forEach { variant ->
                 "versionName" to "v$verName",
                 "versionCode" to verCode,
                 "authorList" to authors,
-                "updateJson" to "https://lsposed.github.io/LSPosed/release/${flavorLowered}.json",
+                "updateJson" to "https://raw.githubusercontent.com/CMDQ8575/LSPosed/master/${flavorLowered}.json",
                 "requirement" to when (flavorLowered) {
                     "riru" -> "Requires Riru $moduleMinRiruVersionName or above installed"
                     "zygisk" -> "Requires Magisk 24.0+ and Zygisk enabled"
